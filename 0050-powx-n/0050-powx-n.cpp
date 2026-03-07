44f9ce895ab7b76205bf81fe1d1ef415de19605f
kanishka675 <vibhandikkanishka@gmail.com>
2026-03-07 22:13:32 +0530

    Time: 0 ms (100%), Space: 8.6 MB (44.44%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        long binForm = n;
+
+        if(binForm < 0){
+            x = 1/x;
+            binForm = -binForm;
+        }
+        
+        double ans = 1;
+
+
+        while(binForm > 0){
+            if(binForm % 2 == 1){
+               ans = ans * x;
+                
+            }
+            x = x*x;
+            binForm = binForm /2;
+
+        }; // power is positive
+        return ans;
+    }
+};
