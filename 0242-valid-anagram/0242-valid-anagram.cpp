c7c327e23c30e90789921096ffed8e785a9ed8f0
kanishka675 <vibhandikkanishka@gmail.com>
2026-03-08 14:37:42 +0530

    Time: 0 ms (100%), Space: 9.9 MB (23.63%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.length() != t.length()){
+            return false;
+        }
+
+        vector<int> freq(26,0);
+
+        for(char ch: s){
+            freq[ch - 'a']++;
+        }
+        for(char ch: t){
+            freq[ch - 'a']--;
+        }
+
+        for(int x: freq){
+            if(x!=0){
+                return false;
+            }
+        }
+        return true;
+    }
+};
